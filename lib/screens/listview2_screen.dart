9ab5336d07e6901a3d442a0e429b757963d4dc2f
AlexGalla27 <gallardohocesalex@gmail.com>
2025-10-31 08:35:07 +0100

    listview2

---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+
+  final options = const['Targaryen', 'Stark', 'Lannister' , 'Baratheon'];
+   
+  const Listview2Screen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Listview Tipo 2'),
+        backgroundColor: Colors.orangeAccent
+        ,
+        ),
+      
+      body: ListView(
+        children: [
+          ...options.map((e) => ListTile(
+            trailing: Icon(Icons.arrow_forward_ios_outlined),
+            title: Text(e),
+            ),).toList()
+          
+        ],
+      ),
+    );
+  }
+}
