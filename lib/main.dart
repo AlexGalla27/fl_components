1f3b820eaa5b8debe988e0de49e5de7f5da681d7
AlexGalla27 <gallardohocesalex@gmail.com>
2025-10-31 09:07:31 +0100

    export

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'package:fl_components/screens/alert_screen.dart';
-import 'package:fl_components/screens/card_screen.dart';
-import 'package:fl_components/screens/home_screen.dart';
-import 'package:fl_components/screens/listview1_screen.dart';
-import 'package:fl_components/screens/listview2_screen.dart';
+import 'package:fl_components/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
